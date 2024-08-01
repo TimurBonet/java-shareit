c9c240d1a5ec30dd56d1530a2a5580bd62cce67e
TimurBonet <lightsword@yandex.ru>
2024-08-01 08:55:29 +0700

    ФП14 корректировка чекстайл

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
